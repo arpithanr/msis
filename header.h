bca2c806bbabfb9bad8d8416111829186d160a14
arpithanr <71539085+arpithanr@users.noreply.github.com>
2023-09-01 12:43:15 +0530

    Add files via upload

---
+++ header.h
@@ -0,0 +1,5 @@
+int smallest_number(int *arr, int size);
+
+int scalling(int *arr,int size, int k);
+
+void countOfAB(char *s, int *c1, int *c2);
---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+#include <assert.h>
+
+int main(){
+	//4.smallest array
+	//int small, size=8, i , arr[size];
+	//printf("enter the array elements:\n");
+	//for(int i=0; i<size; i++)
+	//{
+	//	scanf("%d", &arr[i]);
+	//}
+	//printf("The smallest of array is: %d", smallest_number(arr, size));
+
+	//5.scalling
+	//int size = 8, i , arr[size];
+	//printf("Enter the array elements: \n");
+	//for(i = 0; i<size; i++)
+	//{
+	//	scanf("%d", &arr[i]);
+		
+	//}		
+	//scalling(arr, size, 4);
+
+	//6. total number of characters in the string without function
+	//char str[50];
+	//int i, length;
+
+	//printf("Enter a string: \n");
+	//scanf("%s", str);
+
+	//for(i=0; str[i]!='\0'; i++)
+	//	length++;
+
+	//printf("\n Total number of characters in the string: %d", length);
+
+	//7. Write a function which takes in a string as a parameter and finds the total number of character 'a' & 'b' in the string
+	//char s[50];
+	//int c1=0, c2=0;
+	//printf("Enter the string:");
+	//fgets(s, 25, stdin);
+	//countOfAB(s, &c1, &c2);
+
+	
+	return 0;
+
+}
