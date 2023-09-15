3ad6d8f28e1178287c2a64f2d75b00c47eaa4630
arpithanr <71539085+arpithanr@users.noreply.github.com>
2023-09-15 11:56:52 +0530

    Add files via upload

---
+++ RTOS/Assignment2/Assignment3/process6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main()
+{
+	int id;
+	id = fork();
+
+	if(id < 0)
+	{
+		printf("Process not created\n");
+		exit(-1);
+	}else if(id == 0)
+	{
+		execlp("ls","ls","-1",NULL);
+		perror("exec");
+		exit(1);
+	}else{
+		wait(NULL);
+	}
+	return 0;
+}
